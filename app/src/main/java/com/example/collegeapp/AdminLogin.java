e6f556fc0c956e8f5ee7ccaaea0c8458d3733dfe
Koushik <koushik.nov01@gmail.com>
2022-04-29 19:21:45 +0530

    minor bug fixes-2

--- app/src/main/java/com/example/collegeapp/AdminLogin.java
+++ app/src/main/java/com/example/collegeapp/AdminLogin.java
@@ -3,12 +3,17 @@ package com.example.collegeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class AdminLogin extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        // taking FirebaseAuth instance
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
     }
 }
