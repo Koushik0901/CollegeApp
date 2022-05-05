3fff0a7f0cd73eace6b7eda8e412e635499d9789
jeshurun benedict <jeshurunbenedict@Jeshuruns-MacBook-Pro.local>
2022-05-05 21:32:59 +0530

    gallary for notice

--- app/src/main/java/com/example/collegeapp/uploadNotice.java
+++ app/src/main/java/com/example/collegeapp/uploadNotice.java
@@ -1,15 +1,60 @@
 package com.example.collegeapp;
 
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class uploadNotice extends AppCompatActivity {
 
+    private CardView insertimage;
+    private ImageView noticeImageView;
+
+    private final int REQ = 1;
+    private Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_notice);
+
+        insertimage = findViewById(R.id.insertimage);
+        noticeImageView = findViewById(R.id.noticeImageView);
+
+        insertimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGallery();
+            }
+        });
+
+        }
+        private void openGallery() {
+            Intent pickImage = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(pickImage,REQ);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQ && resultCode == RESULT_OK){
+            Uri uri = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            noticeImageView.setImageBitmap(bitmap);
+        }
     }
 }
