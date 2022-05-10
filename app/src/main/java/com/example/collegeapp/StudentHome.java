2a0eb72b2b5fdc3662310ec7b4a80010b2f56cdb
jeshurun benedict <jeshurunbenedict@Jeshuruns-MacBook-Pro.local>
2022-05-10 20:29:08 +0530

    on click listner for viewmarks

--- app/src/main/java/com/example/collegeapp/StudentHome.java
+++ app/src/main/java/com/example/collegeapp/StudentHome.java
@@ -2,7 +2,11 @@ package com.example.collegeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.card.MaterialCardView;
 
 public class StudentHome extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class StudentHome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_home);
+
+        MaterialCardView Mark = (MaterialCardView) findViewById(R.id.Mark);
+
+        Mark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMark();
+            }
+        });
+    }
+    public void openMark() {
+        Intent intent = new Intent(this, viewMark.class);
+        startActivity(intent);
     }
 }
