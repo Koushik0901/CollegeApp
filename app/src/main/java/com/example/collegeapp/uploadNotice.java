02946f30c7d3034e0ee3501af5a1d4d13cb42b59
Koushik <koushik.nov01@gmail.com>
2022-05-25 17:50:11 +0530

    UI updates

--- app/src/main/java/com/example/collegeapp/uploadNotice.java
+++ app/src/main/java/com/example/collegeapp/uploadNotice.java
@@ -5,6 +5,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import org.bson.Document;
@@ -29,8 +31,11 @@ public class uploadNotice extends AppCompatActivity {
     User user;
     App app;
     MongoCollection<Document> mongoCollection;
+
     Button uploadBtn;
-    EditText text;
+    EditText NoticeInput, TitleInput;
+    RadioGroup radioGroup;
+    String category;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,18 +44,35 @@ public class uploadNotice extends AppCompatActivity {
         app = new App(new AppConfiguration.Builder(appId).build());
 
         uploadBtn = (Button) findViewById(R.id.uploadBtn);
-        text = (EditText) findViewById(R.id.editTextTextPersonName);
+        NoticeInput = (EditText) findViewById(R.id.addNotice);
+        TitleInput = (EditText) findViewById(R.id.NoticeTitle);
+        radioGroup = (RadioGroup) findViewById(R.id.RadioGroup);
+
+        radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+                    RadioButton radioButton = (RadioButton) group.findViewById(checkedId);
+        });
+
 
         user = app.currentUser();
         Log.v("AUTH", user + " " + user.getId());
         mongoClient = user.getMongoClient("mongodb-atlas");
         mongoDatabase = mongoClient.getDatabase("CollegeData");
-        mongoCollection = mongoDatabase.getCollection("Attendance");
+        mongoCollection = mongoDatabase.getCollection("Notice");
 
         uploadBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String notice = text.getText().toString();
+                String notice = NoticeInput.getText().toString();
+                String title = TitleInput.getText().toString();
+
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+                if (selectedId == -1) {
+                    category = "casual";
+                }
+                else {
+                    RadioButton radioButton = (RadioButton) radioGroup.findViewById(selectedId);
+                    category = radioButton.getText().toString();
+                }
                 // create a document for find query
                 Document queryFilter = new Document("notice", notice);
                 // init a mongo collection find task
@@ -64,7 +86,7 @@ public class uploadNotice extends AppCompatActivity {
                             Document result = results.next();
                             Toast.makeText(getApplicationContext(), "Already Exists", Toast.LENGTH_LONG).show();
                             // update the result
-                            result.append("notice", notice);
+                            result.append("title", title).append("notice", notice).append("category", category);
                             mongoCollection.updateOne(queryFilter, result).getAsync(updateResult -> {
                                 if (updateResult.isSuccess()) {
                                     Log.v("Update", "Update successful");
@@ -78,7 +100,7 @@ public class uploadNotice extends AppCompatActivity {
                         else {
                             // insert a document to the collection
                             mongoCollection.insertOne(
-                                    new Document("notice", notice)
+                                    new Document("user_id", user.getId()).append("title", title).append("notice", notice).append("category", category)
                             ).getAsync(result -> {
                                 if (result.isSuccess()) {
                                     Log.v("Insert", "Insert successful");
